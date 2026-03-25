b520819621403cd3e7b351ad9c5d75d51bfdb8d7
Tran Nhat Minh <minhmine16@gmail.com>
2026-03-25 08:26:18 +0700

    bai11

---
+++ bai11.py
@@ -0,0 +1,7 @@
+while True:
+    a = int(input("Nhap so nguyen duong:"))
+    if a > 0:
+        print("Bạn nhập đúng quy tắc")
+        break
+    else:
+        print("khong phai so nguyen duong, vui long nhap lai")
